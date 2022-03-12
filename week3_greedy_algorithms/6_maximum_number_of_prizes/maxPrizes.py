2467a19d07a28e85b63133c9772521bae3aa15c9
s403o <eslam.adel.me@gmail.com>
2022-03-12 18:54:04 +0200

    3-6: Maximum Number of Prizes (Different Summands)

---
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/maxPrizes.py
@@ -0,0 +1,49 @@
+######################################################
+#                ▄▄▄     ▄▄▄▄     ▄▄▄▄▄              #
+#               ▄███    ██▀▀██   █▀▀▀▀██▄            #
+#  ▄▄█████▄    █▀ ██   ██    ██       ▄██   ▄████▄   #
+#  ██▄▄▄▄ ▀  ▄█▀  ██   ██ ██ ██    █████   ██▀  ▀██  #
+#   ▀▀▀▀██▄  ████████  ██    ██       ▀██  ██    ██  #
+#  █▄▄▄▄▄██       ██    ██▄▄██   █▄▄▄▄██▀  ▀██▄▄██▀  #
+#   ▀▀▀▀▀▀        ▀▀     ▀▀▀▀     ▀▀▀▀▀      ▀▀▀▀    #
+#                                                    #
+######################################################
+from ast import Global
+import sys
+reader = (s.rstrip() for s in sys.stdin)
+input = reader.__next__
+#---------------------- USER DEFINED INPUT FUNCTIONS ----------------------#
+def inp():
+    return(int(input()))
+def mulinp():
+  return([int(x) for x in input().split()])
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    return(input().strip())
+def invr():
+    return(map(int,input().split()))
+#--------------------------------------------------------------------------#
+
+def sol(n):
+  candies = n
+  prizes = []
+  for i in range(1, n + 1):
+    if(i + (i + 1)) <= candies:
+      prizes.append(i)
+      candies -= i
+    else:
+      prizes.append(candies)
+      candies = 0
+    if candies == 0:
+      break
+  return prizes
+
+def main():
+  n = inp()
+  prizes = sol(n)
+  print(len(prizes))
+  print(' '.join([str(i) for i in prizes]))
+
+if __name__ == '__main__':
+    main()
