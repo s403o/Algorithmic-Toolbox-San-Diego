2ca90b87067f3063a281124cbbf5617959f7681a
s403o <eslam.adel.me@gmail.com>
2022-06-03 18:18:17 +0200

    4-3: Improving QuickSort

---
+++ week4_divide_and_conquer/3_improving_quicksort/QuickSort.py
@@ -0,0 +1,39 @@
+######################################################
+#                ▄▄▄     ▄▄▄▄     ▄▄▄▄▄              #
+#               ▄███    ██▀▀██   █▀▀▀▀██▄            #
+#  ▄▄█████▄    █▀ ██   ██    ██       ▄██   ▄████▄   #
+#  ██▄▄▄▄ ▀  ▄█▀  ██   ██ ██ ██    █████   ██▀  ▀██  #
+#   ▀▀▀▀██▄  ████████  ██    ██       ▀██  ██    ██  #
+#  █▄▄▄▄▄██       ██    ██▄▄██   █▄▄▄▄██▀  ▀██▄▄██▀  #
+#   ▀▀▀▀▀▀        ▀▀     ▀▀▀▀     ▀▀▀▀▀      ▀▀▀▀    #
+#                                                    #
+######################################################
+import sys
+
+reader = (s.rstrip() for s in sys.stdin)
+input = reader.__next__
+#---------------------- USER DEFINED INPUT FUNCTIONS ----------------------#
+def inp():
+    return(int(input()))
+def mulinp():
+  return([int(x) for x in input().strip().split()])
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    return(input().strip())
+def invr():
+    return(map(int,input().split()))
+#--------------------------------------------------------------------------#
+
+def sol(arr):
+  sort = sorted(arr)
+  return sort
+          
+def main():
+  global n, arr
+  n = inp()
+  arr = mulinp()
+  print(*sol(arr))
+
+if __name__ == '__main__':
+    main()
