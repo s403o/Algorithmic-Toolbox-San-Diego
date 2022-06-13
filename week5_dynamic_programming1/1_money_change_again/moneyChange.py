601f885614a3a1206908c5697d0e760f6a05e97a
s403o <eslam.adel.me@gmail.com>
2022-06-13 16:29:31 +0200

    5-1: Money Change Again

---
+++ week5_dynamic_programming1/1_money_change_again/moneyChange.py
@@ -0,0 +1,46 @@
+######################################################
+#                ▄▄▄     ▄▄▄▄     ▄▄▄▄▄              #
+#               ▄███    ██▀▀██   █▀▀▀▀██▄            #
+#  ▄▄█████▄    █▀ ██   ██    ██       ▄██   ▄████▄   #
+#  ██▄▄▄▄ ▀  ▄█▀  ██   ██ ██ ██    █████   ██▀  ▀██  #
+#   ▀▀▀▀██▄  ████████  ██    ██       ▀██  ██    ██  #
+#  █▄▄▄▄▄██       ██    ██▄▄██   █▄▄▄▄██▀  ▀██▄▄██▀  #
+#   ▀▀▀▀▀▀        ▀▀     ▀▀▀▀     ▀▀▀▀▀      ▀▀▀▀    #
+#                                                    #
+######################################################
+import sys, math
+
+reader = (s.rstrip() for s in sys.stdin)
+input = reader.__next__
+#---------------------- USER DEFINED INPUT FUNCTIONS ----------------------#
+def inp():
+    return(int(input()))
+def mulinp():
+  return([int(x) for x in input().strip().split()])
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    return(input().strip())
+def invr():
+    return(map(int,input().split()))
+#--------------------------------------------------------------------------#
+
+def sol(money):
+  coins = [1, 3, 4]
+  minCoins = [0] + [math.inf] * money
+
+  for i in range(1, money + 1):
+    for coin in coins:
+      if i >= coin:
+        nCoins = minCoins[i - coin] + 1
+        if nCoins < minCoins[i]:
+          minCoins[i] = nCoins
+  
+  return minCoins[money]
+          
+def main():
+  n = inp()
+  print(sol(n))
+
+if __name__ == '__main__':
+    main()
